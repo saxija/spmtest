216a260a455c74f4a8658f2754d744980bf539a5
Sasa Jovanovic <1303sasa@gmail.com>
2022-07-07 14:31:47 +0200

    First commit

---
+++ Sources/SPMTestPackage/SPMTestPackage.swift
@@ -0,0 +1,11 @@
+public struct SPMTestPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+    
+    public func testPrint() {
+        print("ALooo pa ovo kao radiiiii")
+
+    }
+}
